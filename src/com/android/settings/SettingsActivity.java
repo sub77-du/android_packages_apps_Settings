1f39589da6c3cd4e2bef4f31db3302758f9792fa
deadman96385 <seanhoyt963@gmail.com>
2015-02-25 21:51:05 -0500

    Show Kernel Auditor in Dashboard, if installed
    Change-Id: I9486b963ebe575830db9ae551f4c213c801f2c6e
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    Signed-off-by: sub77 <sub77@ymail.com>
    
    Conflicts:
    	res/xml/dashboard_categories.xml

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1323,16 +1323,7 @@ public class SettingsActivity extends Activity
                                         PackageManager.FEATURE_NFC_HOST_CARD_EMULATION)) {
                             removeTile = true;
                         }
-                 } else if (id == R.id.viper_settings) {
-                     boolean supported = false;
-                     try {
-                         supported = (getPackageManager().getPackageInfo("com.vipercn.viper4android_v2", 0).versionCode > 0);
-                     } catch (PackageManager.NameNotFoundException e) {
-                     }
-                     if (!supported) {
-                         removeTile = true;
-                     }
-                    }
+                   }
                 } else if (id == R.id.print_settings) {
                     boolean hasPrintingSupport = getPackageManager().hasSystemFeature(
                             PackageManager.FEATURE_PRINTING);
@@ -1360,6 +1351,25 @@ public class SettingsActivity extends Activity
                     try {
                         supported = (getPackageManager().getPackageInfo("eu.chainfire.supersu", 0).versionCode >= 185);
                     } catch (PackageManager.NameNotFoundException e) {
+                    }
+                    if (!supported) {
+                        removeTile = true;
+                    }
+                } else if (id == R.id.viper_settings) {
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("com.vipercn.viper4android_v2", 0).versionCode > 0);
+                    } catch (PackageManager.NameNotFoundException e) {
+                    }
+                    if (!supported) {
+                        removeTile = true;
+                    }
+                } else if (id == R.id.kernel_auditor) {
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("com.grarak.kerneladiutor", 0).versionCode > 0);
+                    } catch (PackageManager.NameNotFoundException e) {
+
                     }
                     if (!supported) {
                         removeTile = true;
